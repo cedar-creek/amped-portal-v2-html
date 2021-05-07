313d8635da99bf1cbc12ebc107176ee691b8076c
speedwheel <edi.ultras@gmail.com>
2021-05-07 13:21:14 +0300

    add templates +golang

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import "github.com/kataras/iris/v12"
 func main() {
 	app := iris.New()
 	app.RegisterView(iris.HTML("./layouts", ".html").
+		Layout("shared/layout.html").
 		Reload(true))
 
 	app.HandleDir("/assets", iris.Dir("./demo1/dist/assets"))
 
 	app.Get("/", func(ctx iris.Context) {
-		ctx.View("index.html")
+		ctx.View("index.html") 
 	})
 
 	app.Listen(":8080")
