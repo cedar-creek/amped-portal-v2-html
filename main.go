dd509fdbe630b36da28f4b7a6e10843272aef4bf
speedwheel <edi.ultras@gmail.com>
2021-05-07 05:56:23 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.New()
+	app.RegisterView(iris.HTML("./layouts", ".html").
+		Reload(true))
+
+	app.HandleDir("/assets", iris.Dir("./demo1/dist/assets"))
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.View("index.html")
+	})
+
+	app.Listen(":8080")
+}
